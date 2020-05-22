6ed9f8c97a67c3b160d4fbf1bab42c458fdef504
waytothevenus <andyhung772@gmail.com>
2020-05-22 14:08:21 -0700

    maybe

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,8 @@
 import Head from "next/head"
 import Link from 'next/link'
 
-import BannerLanding from '../components/BannerLanding'
 import Layout from '../components/layout'
+import BannerLanding from '../components/BannerLanding'
 
 export default () => (
     <Layout>
