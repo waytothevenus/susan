ca4f75cad3138fddca3141e8869f50330af85abe
waytothevenus <andyhung772@gmail.com>
2022-01-27 09:02:52 -0800

    add uppy

--- components/Callout.js
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link'
-
-const Callout = (props) => (
-    <section id="callout">
-        <Link href="/covid"><a className="copy">Online therapy sessions available during COVID-19</a></Link>
-    </section>
-)
-
-export default Callout
--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-import '../styles/skeleton.css'
-import '../styles/main.scss'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
