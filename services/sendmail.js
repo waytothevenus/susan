f96a223509ccb666cb12e914a6aa3d2e60906d6f
waytothevenus <andyhung772@gmail.com>
2022-07-26 01:17:26 -0700

    teweka

--- services/sendmail.js
+++ services/sendmail.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const handler = async(params) => {
     params = new URLSearchParams(params)
-    return await axios.post('/io.php', params)
+    return await axios.post('http://lacymorrow.com/clients/susanmorrow/io.php', params, {headers: "Access-Control-Allow-Origin: *"})
         .then(function(response) {
             console.log('response: ', response);
             return response
