c05549bca9f781f12d6a0c09147c31af62ac741e
waytothevenus <andyhung772@gmail.com>
2023-05-26 17:49:50 -0400

    adalytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,13 @@
 import '../styles/skeleton.css'
 import '../styles/main.scss'
+import { Analytics } from '@vercel/analytics/react';
+
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Analytics>
+      <Component {...pageProps} />
+    </Analytics>
+  )
 }
 export default App
