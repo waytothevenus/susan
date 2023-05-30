cb52b3cc298ac770f7690033248e263b8b49e273
waytothevenus <andyhung772@gmail.com>
2023-05-30 18:21:35 -0400

    try word split

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -2,7 +2,7 @@ const Banner = (props) => (
 	<section id="banner" className="major">
 		<div className="inner">
 			<header className="major">
-				<h1>Embrace Change: Experience <wbr />Therapy that Delivers</h1>
+				<h1><span className="avoidwrap">Embrace Change: Experience </span> <span className="avoidwrap"> Therapy that Delivers </span></h1>
 			</header>
 			<div className="content">
 				{/* <p>In-person Therapy and telehealth visits in Charlotte and across NC.</p> */}
