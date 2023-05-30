dc654bd768388a8c34ca21166dc4246fb92796e8
waytothevenus <andyhung772@gmail.com>
2023-05-29 23:13:30 -0400

    copy changes

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -2,10 +2,11 @@ const Banner = (props) => (
 	<section id="banner" className="major">
 		<div className="inner">
 			<header className="major">
-				<h1>Transforming Lives <br />through Lasting Love</h1>
+				<h1>Embrace Change: Experience Therapy that Delivers</h1>
 			</header>
 			<div className="content">
-				<p>In-person Therapy and telehealth visits in Charlotte and across NC.</p>
+				{/* <p>In-person Therapy and telehealth visits in Charlotte and across NC.</p> */}
+				<p>30 years experience delivering compassionate, personalized counseling strategies.</p>
 				<ul className="actions">
 					<li><a href="#contact" className="button next scrolly">Schedule an appointment</a></li>
 				</ul>
