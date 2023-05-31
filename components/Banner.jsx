c483977099aa061a45fb2ffc2bed6198203925ad
waytothevenus <andyhung772@gmail.com>
2023-05-31 19:55:47 -0400

    update header

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -2,11 +2,12 @@ const Banner = (props) => (
 	<section id="banner" className="major">
 		<div className="inner">
 			<header className="major">
-				<h1><span className="avoidwrap">Embrace Change: Experience </span> <span className="avoidwrap"> Therapy that Delivers </span></h1>
+				<h1><span className="avoidwrap">Unlock Softer Emotions, Build Loving Relationships. </span> <span className="avoidwrap"> Experience the Power of Emotionally Focused Therapy. </span></h1>
 			</header>
 			<div className="content">
 				{/* <p>In-person Therapy and telehealth visits in Charlotte and across NC.</p> */}
-				<p>30 years experience delivering compassionate, personalized counseling strategies.</p>
+				{/* <p>30 years experience delivering compassionate, personalized counseling strategies.</p> */}
+				<p>Compassionate, Personalized, Expert Therapy Services for Individuals, and Couples.</p>
 				<ul className="actions">
 					<li><a href="#contact" className="button next scrolly">Schedule an appointment</a></li>
 				</ul>
