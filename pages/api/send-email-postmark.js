6174c88fc1ac889a84b3a4fad4f94dff77c9563d
waytothevenus <andyhung772@gmail.com>
2023-06-04 18:10:19 -0400

    rollback spam filter

--- pages/api/send-email-postmark.js
+++ pages/api/send-email-postmark.js
@@ -3,11 +3,14 @@ import { Configuration, OpenAIApi } from 'openai'
 
 const handler = async (req, res) => {
   if (req.method === 'POST') {
+
+		const sendEmail = true // set to false to skip sending email; for testing purposes
     const { name, email, message } = req.body;
 
     const HtmlBody = `<h1>Contact from = </h1><p><b>${name}</b> justsaid:</p><p>${message}</p><p>${email}</p>`;
     const TextBody = `Contact from ${name}: \r\n${email}\r\n${message}`;
 
+		let isSpam = ''
     let spamPrefix = '❌'
     // ai spam check
     let isAISpam = false
@@ -21,15 +24,55 @@ const handler = async (req, res) => {
         'X-Postmark-Server-Token': process.env.POSTMARK_TOKEN,
       },
       body: JSON.stringify({
-        email: `Contact from ${name}: \r\n${email}\r\n${message}`,
-				message: `Contact from ${name}: \r\n${email}\r\n${message}`,
+				options: 'short',
+        email: `Contact from ${name}:\r\nEmail:\r\n${email}\r\nMessage:\r\n${message}`,
       }),
     })
-      .then((response) => response.json())
-      .then((data) => data)
+      .then(async (response) => {
+				const data = await response.json()
+				console.log(data.score)
+				// {
+				// 	success: true,
+				// 	score: '7.9',
+				// 	rules: [
+				// 		{ score: '1.2', description: 'Missing To: header' },
+				// 		{
+				// 			score: '-0.0',
+				// 			description: 'Informational: message was not relayed via SMTP'
+				// 		},
+				// 		{ score: '0.1', description: 'Missing Message-Id: header' },
+				// 		{ score: '1.0', description: 'Missing From: header' },
+				// 		{
+				// 			score: '2.3',
+				// 			description: 'Message appears to have no textual parts'
+				// 		},
+				// 		{ score: '1.4', description: 'Missing Date: header' },
+				// 		{ score: '1.8', description: 'Missing Subject: header' },
+				// 		{
+				// 			score: '-0.0',
+				// 			description: 'Informational: message has no Received headers'
+				// 		},
+				// 		{
+				// 			score: '0.0',
+				// 			description: 'Message appears to be missing most RFC-822 headers'
+				// 		}
+				// 	],
+				// 	report: ' pts rule                   description                                       \n' +
+				// 		'---- ---------------------- --------------------------------------------------\n' +
+				// 		' 1.2 MISSING_HEADERS        Missing To: header                                \n' +
+				// 		'-0.0 NO_RELAYS              Informational: message was not relayed via SMTP   \n' +
+				// 		' 0.1 MISSING_MID            Missing Message-Id: header                        \n' +
+				// 		' 1.0 MISSING_FROM           Missing From: header                              \n' +
+				// 		' 2.3 EMPTY_MESSAGE          Message appears to have no textual parts          \n' +
+				// 		' 1.4 MISSING_DATE           Missing Date: header                              \n' +
+				// 		' 1.8 MISSING_SUBJECT        Missing Subject: header                           \n' +
+				// 		'-0.0 NO_RECEIVED            Informational: message has no Received headers    \n' +
+				// 		' 0.0 NO_HEADERS_MESSAGE     Message appears to be missing most RFC-822 headers'
+				// }
+				return data.score
+			})
 
-      console.log(`Postmark Spam Check: ${console.dir(isPostSpam)}`);
-
+    console.log(`Postmark Spam Check: ${console.dir(isPostSpam)}`);
 
       // if (isPostSpam && spamPrefix) {
     //   return res.status(400).json({
@@ -50,8 +93,6 @@ const handler = async (req, res) => {
           },
         });
     }
-
-    console.log(`Prompt content: ${TextBody}`)
 
     const configuration = new Configuration({
       apiKey: process.env.OPENAI_API_KEY,
@@ -77,13 +118,31 @@ const handler = async (req, res) => {
       frequency_penalty: 0,
       presence_penalty: 0,
       stop: ["\n"],
-    });
+    })
+		// .then((response) => response.json())
+		.then((response) => {
+			if(response.status < 400) {
+				console.log(response.data)
+				throw new Error(response)
+			}
 
-    const data = response.data.choices[0].text;
-    console.log(data);
+			console.log(response.request.data)
+			// const data = response.data.choices[0].text;
+			// let isSpam = data === 'spam' ? '[❌ AI: SPAM]' : ''
 
-		let isSpam = data === 'spam' ? '[❌ AI: SPAM]' : ''
-		isSpam = (!isSpam && isPostSpam ) ? '[❌ MAYBE: SPAM]' : ''
+			// return data
+			return response
+		})
+		.catch((error) => {
+			console.dir(error);
+			return error
+		});
+
+		// let isSpam = data === 'spam' ? '[❌ AI: SPAM]' : ''
+
+
+
+		// isSpam = (!isSpam && isPostSpam ) ? '[❌ MAYBE: SPAM]' : ''
 
     // // if (data === 'true') {
     // //   return res.status(400).json({
@@ -95,26 +154,35 @@ const handler = async (req, res) => {
     // // }
 
     // Send an email via postmark:
-		const subject = `${isSpam || '👻'} SusanMorrow.us Inquiry: ${name}`
+		if(sendEmail) {
+			const subject = `${isSpam || '👻'} SusanMorrow.us Inquiry: ${name}`
 
-    const postmarkClient = new Client(process.env.POSTMARK_TOKEN || '');
+			const postmarkClient = new Client(process.env.POSTMARK_TOKEN || '');
 
-    const emailResponse = await postmarkClient.sendEmail({
-      From: 'susan@susanmorrow.us',
-      To: process.env.RECEIVING_EMAIL || 'me@lacymorrow.com',
-      Subject: subject,
-      HtmlBody,
-      TextBody,
-      MessageStream: 'outbound',
-    });
+			const emailResponse = await postmarkClient.sendEmail({
+				From: 'susan@susanmorrow.us',
+				To: process.env.RECEIVING_EMAIL || 'me@lacymorrow.com',
+				Subject: subject,
+				HtmlBody,
+				TextBody,
+				MessageStream: 'outbound',
+			});
 
-    console.dir(emailResponse);
-    if(emailResponse.ErrorCode === 0) {
-      return res
-        .status(200)
-        .json({ message: 'Your message was sent, thanks for reaching out  🚀' });
-    }
-  }
+			// console.dir(emailResponse);
+
+			if(emailResponse.ErrorCode === 0) {
+				return res
+					.status(200)
+					.json({ message: 'Your message was sent, thanks for reaching out  🚀' });
+			}
+		} else {
+			// Testing, don't send email
+			return res
+				.status(200)
+				.json({ message: 'You are in test mode, your message was not sent  🚀' });
+		}
+  } // POST Request
+
   return res.status(404).json({
     error: {
       code: 'not_found',
