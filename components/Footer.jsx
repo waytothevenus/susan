89c13bc7b1ddd4781ea872d9a547fe0d6e70748c
waytothevenus <andyhung772@gmail.com>
2023-06-05 17:17:07 -0400

    added yelp

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,19 +4,20 @@
 //<li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
 
 const Footer = (props) => (
-    <footer id="footer">
-        <div className="inner">
+	<footer id="footer">
+		<div className="inner">
 
-            <ul className="icons">
-                <li><a href="https://www.linkedin.com/in/centercharlottepsychotherapy/" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
-                <li><a href="https://www.facebook.com/susan.morrow.148" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-            </ul>
+			<ul className="icons">
+				<li><a href="https://www.linkedin.com/in/centercharlottepsychotherapy/" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
+				<li><a href="https://www.facebook.com/susan.morrow.148" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
+				<li><a href="https://www.yelp.com/biz/charlotte-therapy-and-counseling-susan-morrow-msw-lcsw-acsw-charlotte" className="icon alt fa-yelp"><span className="label">Yelp</span></a></li>
+			</ul>
 
-            <ul className="copyright">
-                <li>&copy; Susan Morrow, MSW, LCSW 2023 | Therapy in Charlotte, NC</li>
-            </ul>
-        </div>
-    </footer>
+			<ul className="copyright">
+				<li>&copy; Susan Morrow, MSW, LCSW 2023 | Therapy in Charlotte, NC</li>
+			</ul>
+		</div>
+	</footer>
 )
 
 export default Footer
